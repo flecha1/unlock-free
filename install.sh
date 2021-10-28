fde5f9338687eadf5b02875b1f9921ce6e9afa3d
flecha1 <91440667+flecha1@users.noreply.github.com>
2021-10-28 12:41:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-apt install unzip
+apt install unzip -y
+apt install bash -y
+apt install unzip -y
+apt install nodejs -y
 wget https://github.com/flecha1/unlock-free/raw/main/DesEncryp.zip
+unzip DesEncryp.zip
 chmod +x *
+./install-dep.sh
